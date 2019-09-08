2c105bf93059e9041abab1cebd5769f38a1db98c
Rosswell Tiongco <rosswelltiongco@gmail.com>
2019-09-08 15:34:01 -0700

    Add basic fan class

--- fan.py
+++ fan.py
@@ -1,43 +1,31 @@
+gpio_pin = 8
+INDEFINITE = 999
 
-import wiringpi
-import time
-wiringpi.pwmSetMode(0) # PWM_MODE_MS = 0
+class Fan:
+    def __init__(self,gpio_pin):
+        """
+        Initialize code
+        """
+        self.fan_pin = gpio_pin
 
-wiringpi.wiringPiSetupGpio()  
+    def get_pin(self):
+        """
+        Returns: Gpio pin
+        """
+        return self.fan_pin
 
-wiringpi.pinMode(18, 2)      # pwm only works on GPIO port 18  
+    def start_fan(self,pwm_value):
+        """
+        Args:
+            pwm_value (int): value to run at
+        
+        Runs indefinitely
+        """
+        print("fan is running at {}".format(pwm_value))
+        print(INDEFINITE)
 
-wiringpi.pwmSetClock(6)  # this parameters correspond to 25kHz --at what clock freq will it run the best
-# only port 18 will work with pwm 
-
-wiringpi.pwmSetRange(128)
-
-print("Press any key to start")
-input()
-time.sleep(2)
-wiringpi.pwmWrite(18, 0)   # minimum RPM
-print("PWM: 0")
-#there needs to be an assertion of delay for the fan to be ready operating at full speed
-
-time.sleep(5)
-wiringpi.pwmWrite(18, 128)  # maximum RPM
-
-time.sleep(3)
-
-print("PWM: 128")     
-#time.sleep(3)
-#print("PWM: 0") 
-wiringpi.pwmWrite(18, 0)  # maximum RPM
-time.sleep(3)
-
-
-time.sleep(3)
-
-wiringpi.pwmWrite(18, 128)  # maximum RPM
-print("PWM: 128")     
-time.sleep(5)
-print("PWM: 0")
-
-wiringpi.pwmWrite(18, 0)  # maximum RPM
-time.sleep(3)
-print("fisnihed") 
+    def stop_fan(self):
+        """
+        Terminates fan
+        """
+        print("fan stopped")
