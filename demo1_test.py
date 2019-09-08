a77db8daf2dacaf1e97522626c95d53613841ba4
Rosswell Tiongco <rosswelltiongco@gmail.com>
2019-09-08 16:27:03 -0700

    Change order of demo 1

--- demo1_test.py
+++ demo1_test.py
@@ -16,19 +16,30 @@ GPIO.setup(19, GPIO.OUT)
 def main():
     fan_obj = fan.Fan()
     solenoid_obj = solenoid.Solenoid()
-    fan_obj.start_fan(0)
-    
-    solenoid_obj.block()
-    
-    print "Please wait for fan..."
-    fan_obj.start_fan(100)
-    print "Please load ball in on turret\n"
-    
-    time.sleep(5)
-    solenoid_obj.release()
-    print "And we have launch off!!!"
+    quit = False
+    print "Welcome to Demo 1 of King Pong!\n"
+    print "Loading -____-"
     fan_obj.stop_fan()
-
+    while(quit==False):
+        solenoid_obj.release()
+        print "Please wait for fan..."
+        fan_obj.start_fan(100)
+        print "Please load ball in on turret\n"
+        solenoid_obj.block()
+        time.sleep(5)
+        solenoid_obj.release()
+        print "And we have launch off!!!"
+        print "Enter any number to stop fan: "
+        user_key = input()
+        fan_obj.stop_fan()
+        print "Retry? Enter 1: "
+        user_key = input()
+        if(user_key==1):
+            quit = False
+        else:
+            quit = True
+            fan_obj.stop_fan()
+    print "Program Terminated"
     GPIO.cleanup()
 
 
