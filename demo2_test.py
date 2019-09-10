5dd5747ad17b627de2757fa335032b28e4d39717
Mark Aquiapao <mark.aquiapao@student.csulb.edu>
2019-09-10 14:19:17 -0700

    Added demo 2 with OOP

---
+++ demo2_test.py
@@ -0,0 +1,54 @@
+# Import for all
+import RPi.GPIO as GPIO
+import fan
+import solenoid
+import tachRPM
+import time
+import wiringpi
+
+wiringpi.wiringPiSetupGpio()
+wiringpi.pwmSetMode(0) # PWM_MODE_MS = 0
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD) #this cmd is for user to specify pin as number of the board.
+        # Port for Soleniod
+GPIO.setup(19, GPIO.OUT)
+
+def main():
+    fan_obj = fan.Fan()
+    solenoid_obj = solenoid.Solenoid()
+    tach_obj = tachRPM.Tach()
+    quit = False
+    print "Welcome to Demo 1 of King Pong!\n"
+    print "Loading -____-"
+    fan_obj.stop_fan()
+    while(quit==False):
+        solenoid_obj.release()
+        print "Please wait for fan..."
+        fan_obj.start_fan(100)
+        print "Please load ball in on turret\n"
+        solenoid_obj.block()
+        rpm_value = tach_obj.read_rpm()
+        tach_obj.display_rpm(rpm_value)
+        time.sleep(5)
+        solenoid_obj.release()
+        print "And we have launch off!!!"
+        
+        
+        
+        print "Enter any number to stop fan: "
+        
+        user_key = input()
+        fan_obj.stop_fan()
+        print "Retry? Enter 1: "
+        user_key = input()
+        if(user_key==1):
+            quit = False
+        else:
+            quit = True
+            fan_obj.stop_fan()
+    print "Program Terminated"
+    GPIO.cleanup()
+
+
+main() # run main
