5394d4b1499e8bc73a0349289fe5cff283dd03cd
Mark Aquiapao <mark.aquiapao@student.csulb.edu>
2019-09-10 14:06:09 -0700

    Added tach RPM

---
+++ tachRPM.py
@@ -0,0 +1,34 @@
+
+INDEFINITE = 5
+import RPi.GPIO as GPIO
+
+import wiringpi
+import time
+class Fan:
+    def __init__(self):
+        """
+        Initialize code
+        """
+        # Setup GPIO for tach
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(21, GPIO.IN) # Port 21 = GPIO 9
+    
+    def read_rpm(self):
+        """
+        Args:
+            pwm_value (int): value to run at
+        
+        Runs indefinitely
+        """
+        time.sleep(2)
+        return rpm = tach.RPM()
+
+    def display_rpm(self, rpm_value)
+        """
+        Args:
+        rpm_value (int): value from tach
+            
+        Runs indefinitely
+        """
+        print "RPM: ", rpm_value
+
