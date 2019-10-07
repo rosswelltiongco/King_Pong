e59f8a4547b01c901e82e87de7b1cd693a64419d
trieuvyle <trieuvy.le@student.csulb.edu>
2019-10-07 07:41:39 +0100

    Remove unnecessary wiringpi

--- distance_test.py
+++ distance_test.py
@@ -1,34 +1,28 @@
-# Import for all
-import RPi.GPIO as GPIO
-import fan
-import solenoid
-import time
-import wiringpi
+"""
+TODO ??: calib = 45 # Todo: Maybe use calibration for pwm_distance?
+"""
 
-wiringpi.wiringPiSetupGpio()
-wiringpi.pwmSetMode(0) # PWM_MODE_MS = 0
+import RPi.GPIO as GPIO
+from lib.Fan import *
+from lib.Solenoid import *
+import time
+
+fan = Fan()
+solenoid = Solenoid()
 
 def main():
-    fan_obj = fan.Fan()
-    solenoid_obj = solenoid.Solenoid()
-    
-    calib = 45
-
     
     while 1:
         pwm_distances = [42, 45, 48, 51]
         selected_cup = int(input("Enter cup")) - 1
         pwm = pwm_distances[selected_cup]
-        solenoid_obj.block()
-        fan_obj.set_pwm(pwm)
+        solenoid.block()
+        fan.set_pwm(pwm)
         time.sleep(3)
-        solenoid_obj.release()
+        solenoid.release()
         time.sleep(1) # Follow through
-        fan_obj.set_pwm(0)
+        fan.set_pwm(0)
         
     GPIO.cleanup()
 
-
-
-
-main() # run main
+main()
