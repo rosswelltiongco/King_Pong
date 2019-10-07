8fa6c1572202efef1dce36240f561e7aa340ccc3
Javier952 <55065497+Javier952@users.noreply.github.com>
2019-10-06 18:07:43 -0700

    Update limit_switch.py

--- limit_switch.py
+++ limit_switch.py
@@ -2,21 +2,29 @@
 
 import time
 import pigpio
+import RPi.GPIO as GPIO
 
 pi = pigpio.pi()
 freq=5000
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(22,GPIO.IN)
+def int resetPos:
+   
+   if not pi.connected:
+      exit()
 
-if not pi.connected:
-   exit()
+   for pwm in range(500000, 1100000, 200000): # start from 50% and go up to 110% PWM
+      pi.hardware_PWM(18, freq, pwm)
+      print("\n12 set {} got {} pwm={}%".format(
+      freq, pi.get_PWM_frequency(18),pwm/10000))
+      time.sleep(5)
 
-for pwm in range(500000, 1100000, 200000): # start from 50% and go up to 110% PWM
-   pi.hardware_PWM(18, freq, pwm)
-   print("\n12 set {} got {} pwm={}%".format(
-     freq, pi.get_PWM_frequency(18),pwm/10000))
-   time.sleep(5)
+   pi.hardware_PWM(18, 0, 0)
 
-pi.hardware_PWM(18, 0, 0)
+   pi.stop()
+   pos = 0
+   return pos
 
-pi.stop()
+GPIO.add_event_detect(22,GPIO.RISING,resetPos)
 ~
 ~
