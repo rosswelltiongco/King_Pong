30bded225f540d87fa35b3d8ea27ee1bc91f5900
trieuvyle <trieuvy.le@student.csulb.edu>
2019-10-13 03:40:25 +0100

    Better step code

--- base_test.py
+++ base_test.py
@@ -1,163 +1,25 @@
-import RPi.GPIO as GPIO
-import time     
-GPIO.setwarnings(False)
+
+
+from lib.Base import *
 
 
 
-GPIO.setmode(GPIO.BOARD) #this cmd is for user to specify pin as number of the board.
-# Pins [1,3,4,2] -> 1 = Outer Left, 4 = Outer right, Heat Sink facing down on H-Bridge
-control_pins_left = [15,11,7,13]
-control_pins_right = [10,5,16,8]
-
-LimitSwitchUp = 22
-
-GPIO.setup(LimitSwitchUp, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-#global rightBoundry,leftBoundry, pos
-
-halfstep_forward = [
-           [1,0,0,0],
-           [1,1,0,0],
-           [0,1,0,0],
-           [0,1,1,0],
-           [0,0,1,0],
-           [0,0,1,1],
-           [0,0,0,1],
-           [1,0,0,1]
-        ]
-
-halfstep_reverse = [
-           [1,0,0,1],
-           [0,0,0,1],
-           [0,0,1,1],
-           [0,0,1,0],
-           [0,1,1,0],
-           [0,1,0,0],
-           [1,1,0,0],
-           [1,0,0,0]
-    ]
+def main():
     
-class Base:
-    def __init__(self, pos):    
+    base = Base(0)
+    base.step_left(5)
+    base.step_left(15)
     
-        self.pos = pos
-        self.boundary_left = 400
-        self.boundary_right = 0
-
-
-        for pin in control_pins_left:
-                GPIO.setup(pin, GPIO.OUT)
-                GPIO.output(pin, 0)
-            
-        for pin in control_pins_right:
-            GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin, 0)
-        
-        GPIO.setup(22,GPIO.IN)
-        while(GPIO.input(LimitSwitchUp)==0): # 90 degrees
-            for halfstep in range(8):
-                for pin in range(4):
-                    GPIO.output(control_pins_left[pin], halfstep_forward[halfstep][pin])
-                    GPIO.output(control_pins_right[pin], halfstep_forward[halfstep][pin])
-                time.sleep(0.01)
-
-        #print(leftBoundry)
-
-
-    def move_right(self, delta):
-        """
-        print("Moving right {}".format(delta))
-        for step in range(delta):
-            if self.pos <= self.boundary_right:
-                break # pass or break
-            else:
-                self.pos -= 1
-            print(self.pos)
-
+    base.step_right(20)
     
-        Args:
-            - steps (int): value to move stepper motor in desired position
-        
-        Runs based on x in the right direction
-        """
-        
-        if(delta > self.boundary_right):
-            delta = self.pos 
-        elif (delta < self.boundary_right):
-            delta = delta
-            
-            
-        print(self.pos)
-        print("Base is Turning!")
-        
-        for i in range(delta): # 90 degrees
-            print(self.pos)
-            for halfstep in range(8):
-                for pin in range(4):
-                    GPIO.output(control_pins_left[pin], halfstep_forward[halfstep][pin])
-                    GPIO.output(control_pins_right[pin], halfstep_forward[halfstep][pin])
-                time.sleep(0.01)
-            self.pos -= 1
-        for pin in control_pins_left:
-            GPIO.output(pin, 0)
-            
-        for pin in control_pins_right:
-            GPIO.output(pin, 0)
-
-
-        
-    def move_left(self, delta):
-
-        """
-        Args:
-            - steps (int): value to move stepper motor in desired position
-        
-        Runs based on x in the right direction
-       
-        
-        
-        print("Moving left {}".format(delta))
-        
-        
-        for step in range(delta):
-            
-            if self.pos >= self.boundary_left:
-                break # pass or break
-            else:
-                self.pos += 1
-            print(self.pos)
-        #print("Base is Turning!")
-            
-        
-        """
-        if(self.pos + delta  > self.boundary_left):
-            delta = (self.pos + delta)  - (delta - self.boundary_left) 
-        #elif (self.pos + delta):
-         #   delta = delta - self.pos
-        
-        for i in range(delta): # 90 degrees
-            print(self.pos)
-            for halfstep in range(8):
-                for pin in range(4):
-                    GPIO.output(control_pins_left[pin], halfstep_reverse[halfstep][pin])
-                    GPIO.output(control_pins_right[pin], halfstep_reverse[halfstep][pin])
-                time.sleep(0.01)
-            self.pos += 1
-            
-        for pin in control_pins_left:
-            GPIO.output(pin, 0)
-            
-        for pin in control_pins_right:
-            GPIO.output(pin, 0) 
-
-        
-
-#base = Base(0)
-#base.move_left(75)
-#base.move_right(50)
-
-"""
-base.move_right(150)
-base.move_left(200)
-base.move_right(200)
-"""
+    base.step_left(100)
+    
+    base.step_right(25)
+    
+    base.step_left(10)
+    
+    base.step_right(100)
+    
+    while 1:
+        pass
+main()
--- lib/Base.py
+++ lib/Base.py
@@ -1,16 +1,15 @@
 import RPi.GPIO as GPIO
-import time
+import time     
+GPIO.setwarnings(False)
+
 
 GPIO.setmode(GPIO.BOARD) #this cmd is for user to specify pin as number of the board.
 # Pins [1,3,4,2] -> 1 = Outer Left, 4 = Outer right, Heat Sink facing down on H-Bridge
 control_pins_left = [15,11,7,13]
-control_pins_right = [10,5,16,8]
 
 LimitSwitchUp = 22
 
 GPIO.setup(LimitSwitchUp, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-global rightBoundry,leftBoundry, pos
 
 halfstep_forward = [
            [1,0,0,0],
@@ -32,105 +31,79 @@ halfstep_reverse = [
            [0,1,0,0],
            [1,1,0,0],
            [1,0,0,0]
-        ]
-
-class Base:
+    ]
     
-    def __init__(self):
-        global pos
-        global leftBoundry
-        global rightBoundry
-        for pin in control_pins_left:
-            GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin, 0)
-            
-        for pin in control_pins_right:
-            GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin, 0)
+class Base:
+    def __init__(self, pos):    
+    
+        self.pos = pos
+        self.BOUND_LEFT = 30 #400
+        self.BOUND_RIGHT = 0
+
         
+        
+        """
         GPIO.setup(22,GPIO.IN)
-        leftBoundry = 0
         while(GPIO.input(LimitSwitchUp)==0): # 90 degrees
             for halfstep in range(8):
                 for pin in range(4):
                     GPIO.output(control_pins_left[pin], halfstep_forward[halfstep][pin])
-                    GPIO.output(control_pins_right[pin], halfstep_forward[halfstep][pin])
                 time.sleep(0.01)
-                
-                leftBoundry = leftBoundry + 1
-        rightBoundry = 0
-        leftBoundry = 100
-        pos = 0
-        print(leftBoundry)
-      
-        
-        
-    def move_right(self, steps): 
-        
-        """
-        Args:
-            - steps (int): value to move stepper motor in desired position
-        
-        Runs based on x in the right direction
         """
         
-        print("Base is Turning!")
         
-        for i in range(steps): # 90 degrees
-            for halfstep in range(8):
-                for pin in range(4):
-                    GPIO.output(control_pins_left[pin], halfstep_forward[halfstep][pin])
-                    GPIO.output(control_pins_right[pin], halfstep_forward[halfstep][pin])
-                time.sleep(0.0001)
+    def step_right(self, steps):
+        """
+        if(delta > self.boundary_right):
+            delta = self.pos 
+        elif (delta < self.boundary_right):
+            delta = delta
+        """
+        
+        self.on()
+        
+        
+        for i in range(steps):
+            if self.pos <= self.BOUND_RIGHT:
+                break
+            else:
+                for halfstep in range(8):
+                    for pin in range(4):
+                        GPIO.output(control_pins_left[pin], halfstep_forward[halfstep][pin])
+                    time.sleep(0.01)
+                self.pos -= 1
+            print(self.pos)
+        self.off()
 
-        for pin in control_pins_left:
-            GPIO.output(pin, 0)
-            
-        for pin in control_pins_right:
-            GPIO.output(pin, 0)
-            
-            
-    def move_left(self, steps):
-        global pos
-        global leftBoundry
-        global rightBoundry
-        """
-        Args:
-            - steps   (int): value to move stepper motor in desired position
+
         
-        Runs based on x in left direction
+    def step_left(self, steps):
         """
-        currPos = 0
-        while (GPIO.input(LimitSwitchUp)==0) or (pos > 100):
-            for i in range(steps): # 90 degrees
+        if(self.pos + delta  > self.boundary_left):
+            delta = (self.pos + delta)  - (delta - self.boundary_left) 
+           
+        """
+        
+        self.on()
+        
+            
+        for i in range(steps):
+            if self.pos >= self.BOUND_LEFT:
+                break
+            else:
                 for halfstep in range(8):
                     for pin in range(4):
                         GPIO.output(control_pins_left[pin], halfstep_reverse[halfstep][pin])
-                        GPIO.output(control_pins_right[pin], halfstep_reverse[halfstep][pin])
-                    time.sleep(0.004)
-                pos = pos + 1
-                currPos = pos
-                print(pos)
-
-        for pin in control_pins_left:
-            GPIO.output(pin, 0)
-            
-        for pin in control_pins_right:
-            GPIO.output(pin, 0)
-
-    
-    
-    def stop_base(self, steps):
+                    time.sleep(0.01)
+                self.pos += 1
+            print(self.pos)
+        self.off()
         
-        for i in range(steps): # 90 degrees
-            for halfstep in range(8):
-                for pin in range(4):
-                    GPIO.output(control_pins_left[pin], halfstep_reverse[halfstep][pin])
-                    GPIO.output(control_pins_right[pin], halfstep_reverse[halfstep][pin])
-                time.sleep(0.01)
-
+    def on(self):
         for pin in control_pins_left:
-            GPIO.output(pin, 0)
-            
-        for pin in control_pins_right:
+                GPIO.setup(pin, GPIO.OUT)
+                GPIO.output(pin, 0)
+    
+    def off(self):
+        for pin in control_pins_left:
             GPIO.output(pin, 0)
