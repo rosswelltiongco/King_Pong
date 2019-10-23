936cc1632ef4fbf33ad08155fc4e25c025fce39c
Mark Aquiapao <mark.aquiapao@student.csulb.edu>
2019-10-23 14:45:39 -0700

    Reference for Pi UART

--- UART_ref.py
+++ UART_ref.py
@@ -5,6 +5,7 @@ http://www.electronicwings.com
 import serial
 from time import sleep
 
+'''
 ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 while True:
     received_data = ser.read()              #read serial port
@@ -13,3 +14,15 @@ while True:
     received_data += ser.read(data_left)
     print (received_data)                   #print received data
     ser.write(received_data)                #transmit data serially
+'''
+    
+    
+ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
+while True:
+    ser.write("rnSay something:")         #transmit data serially
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+    ser.write("rnSay something:" + recieved_data) #transmit data serially
