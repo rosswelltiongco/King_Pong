b01b1893a5d2ae1a423bc5d7f64e5b3229bde0d2
Mark Aquiapao <mark.aquiapao@student.csulb.edu>
2019-10-23 15:03:23 -0700

    Simple test for UART

--- UART_test.py
+++ UART_test.py
@@ -1,15 +1,15 @@
 '''
-UART communication on Raspberry Pi using Pyhton
-http://www.electronicwings.com
+The Pi will send the desired RPM and recieve flag to launch the ball
 '''
 import serial
 from time import sleep
 
 ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 while True:
+    ser.write("Hello!")                     #transmit data serially
+    # Should print out "Bye!"
     received_data = ser.read()              #read serial port
     sleep(0.03)
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
     print (received_data)                   #print received data
-    ser.write(received_data)                #transmit data serially
