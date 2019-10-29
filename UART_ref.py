05712f0829677b20ddde0a04b9aecb5132eb5ae4
trieuvyle <trieuvy.le@student.csulb.edu>
2019-10-29 04:48:51 +0000

    be able to read str from tm4c as PWM

--- UART_ref.py
+++ UART_ref.py
@@ -5,7 +5,7 @@ http://www.electronicwings.com
 import serial
 from time import sleep
 
-'''
+"""
 ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 while True:
     received_data = ser.read()              #read serial port
@@ -19,10 +19,16 @@ while True:
     
 ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
 while True:
-    ser.write("rnSay something:")         #transmit data serially
+    ser.write("rnSay something:")           #transmit data serially
     received_data = ser.read()              #read serial port
     sleep(0.03)
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
     print (received_data)                   #print received data
     ser.write("rnSay something:" + recieved_data) #transmit data serially
+"""
+
+port = serial.Serial ("/dev/ttyAMA0", 9600)
+while True:
+        yes = port.readline()
+        print(yes)           #read serial port
--- UART_test.py
+++ UART_test.py
@@ -1,15 +1,25 @@
 '''
 The Pi will send the desired RPM and recieve flag to launch the ball
 '''
+
+import RPi.GPIO as GPIO
+
 import serial
 from time import sleep
+import time
 
-ser = serial.Serial ("/dev/ttyS0", 9600)    #Open port with baud rate
+
+ser = serial.Serial ("/dev/ttyS0", 57600 )    #Open port with baud rate
+    
 while True:
-    ser.write("Hello!")                     #transmit data serially
-    # Should print out "Bye!"
+    ser.write(raw_input("Enter"))                  #transmit data serially
+    #Should print out "Bye!"
+    #time.sleep(5)
     received_data = ser.read()              #read serial port
-    sleep(0.03)
+    time.sleep(1)
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
-    print (received_data)                   #print received data
+    print (received_data)      
+    time.sleep(0.1)
+    
+    
