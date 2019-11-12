8b04dfbe5cf20f7d5a558852c49c792c01de8580
trieuvy <trieuvy.le@student.csulb.edu>
2019-11-12 02:22:10 +0000

    uart

--- UART_test.py
+++ UART_test.py
@@ -12,14 +12,16 @@ import time
 ser = serial.Serial ("/dev/ttyS0", 57600 )    #Open port with baud rate
     
 while True:
-    ser.write(raw_input("Enter"))                  #transmit data serially
+    ser.write(raw_input('enter'))                  #transmit data serially
     #Should print out "Bye!"
-    #time.sleep(5)
+    time.sleep(2)
+    print("here")
+    """
     received_data = ser.read()              #read serial port
     time.sleep(1)
     data_left = ser.inWaiting()             #check for remaining byte
     received_data += ser.read(data_left)
     print (received_data)      
     time.sleep(0.1)
-    
+    """
     
--- main_main.py
+++ main_main.py
@@ -13,6 +13,7 @@ import serial
 
 camera = Camera()
 sensor = Sensor()
+ser = serial.Serial ("/dev/ttyS0", 57600 )    #Open port with baud rate
 
 def main():
     while 1:
@@ -21,6 +22,19 @@ def main():
         distance = sensor.get_distance()
         
         print("x = ", midpoint, "  y = ", distance)
+        
+        
+        while True:
+            ser.write(raw_input("Enter"))                  #transmit data serially
+            #Should print out "Bye!"
+            #time.sleep(5)
+            received_data = ser.read()              #read serial port
+            time.sleep(1)
+            data_left = ser.inWaiting()             #check for remaining byte
+            received_data += ser.read(data_left)
+            print (received_data)      
+            time.sleep(0.1)
+        
         
         
     GPIO.cleanup(), 
