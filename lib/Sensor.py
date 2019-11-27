853db368a409afed6f29844bdd5a73d7838ba2f9
trieuvy <34084306+trieuvy@users.noreply.github.com>
2019-11-26 20:42:45 -0800

    Update Sensor.py

--- lib/Sensor.py
+++ lib/Sensor.py
@@ -1,37 +1,43 @@
 import RPi.GPIO as GPIO
-from lib.Base import *
 import time
 import serial
 
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-TRIG = 40
-ECHO = 38
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+
 
 
 class Sensor:
     def __init__(self):
-        pass
+        
+        self.TRIG = 40
+        self.ECHO = 38
+        
+        GPIO.setup(self.TRIG, GPIO.OUT)
+        GPIO.output(self.TRIG, False)
+        
+        GPIO.setup(self.ECHO, GPIO.IN)
             
 
             
     def get_distance(self):
         try:
-            GPIO.output(TRIG, False)
+            
             #print "Waiting For Sensor To Settle"
             time.sleep(3)
             
-            GPIO.output(TRIG, True)
+            GPIO.output(self.TRIG, True)
             time.sleep(0.00001)
-            GPIO.output(TRIG, False)
+            GPIO.output(self.TRIG, False)
         
-            while GPIO.input(ECHO)==0:
-              pulse_start = time.time()
+            while GPIO.input(self.ECHO)==0:
+                pass
+                pulse_start = time.time()
 
-            while GPIO.input(ECHO)==1:
-              pulse_end = time.time()
+            while GPIO.input(self.ECHO)==1:
+                pass
+                pulse_end = time.time()
 
             pulse_duration = pulse_end - pulse_start
 
@@ -39,6 +45,7 @@ class Sensor:
 
             distance = round(distance, 2)
             
+            print(distance)
             return distance
         
         except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
